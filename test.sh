ae4cd97b1448fb85b8b61d24dc66fab439219570
James Yan <840520333@qq.com>
2020-04-23 12:07:45 -0400

    Add files via upload

---
+++ test.sh
@@ -0,0 +1,4 @@
+kubectl apply -f ./new/working-folder-with/util/build_assistant/a0.yaml
+kubectl apply -f ./new/working-folder-with/util/build_assistant/a1.yaml
+kubectl apply -f ./new/working-folder-with/util/build_assistant/a2.yaml
+kubectl apply -f ./new/working-folder-with/util/build_assistant/a3.yaml
